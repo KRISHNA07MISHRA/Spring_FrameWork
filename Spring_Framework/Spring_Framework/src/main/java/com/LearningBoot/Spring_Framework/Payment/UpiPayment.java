e5cdd012c42a80d2ade915cd3346b3e9e2a2af77
krishna nand mishra <krishnanandmishra386@gmail.com>
2026-06-24 13:11:19 +0530

    learning circular dependency, scope, lazy and eager initialization

--- Spring_Framework/Spring_Framework/src/main/java/com/LearningBoot/Spring_Framework/Payment/UpiPayment.java
+++ Spring_Framework/Spring_Framework/src/main/java/com/LearningBoot/Spring_Framework/Payment/UpiPayment.java
@@ -1,10 +1,12 @@
 package com.LearningBoot.Spring_Framework.Payment;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 @Qualifier("upi")
+@Scope("prototype") // jab tak call nahe hoga tab tak new object create nahe hoga
 public class UpiPayment implements PaymentService{
     public void pay(){
         System.out.println("Payment using Upi");
